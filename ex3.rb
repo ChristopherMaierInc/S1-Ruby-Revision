96146958f0889882eb1c40afeac610ebade45840
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-19 19:41:21 +1000

    Inital Commit

---
+++ ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens:" #prints current line to the screen
+
+puts "Hens #{25 + 30 / 6}" #by order of operations, 30 is devided by 6 and then 25 is added
+puts "Roosters #{100 - 25 * 3 % 4}" #as above, the modulus is used to devide and show the remainder. So, 25 * 3, divided by the answer then that tanken away from 100
+
+puts "Now I will count the eggs:" # printed to screen
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # BOMDAS
+
+puts "Is it true that 3 + 2 < 5 - 7?" # prints to screen
+
+puts 3 + 2 < 5 - 7 # calculates the formular first, then shows a T/F statement in regards to the < formula
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
