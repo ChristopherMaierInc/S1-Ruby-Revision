c84cdd85a942d4ff70d4d954003bc593b53dc7c4
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 15:20:01 +1000

    ex12.rb 'Exercise 12: Prompting People for Numbers' complete

--- ex12.rb
+++ ex12.rb
@@ -10,3 +10,9 @@ number = another.to_i
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
+
+
+print "give me another number bro: "
+num2 = gets.chomp.to_f
+
+print "sick bro, here's your change, lolol!: $#{'%.2f' % (num2 / 10)}"
