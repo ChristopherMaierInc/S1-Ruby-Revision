dc5ffebb316ddd6bffdc3bfac01f59a08f662c3e
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 15:45:17 +1000

    ex13.rb INCOMPLETE! ARGV & gets.chomp not playing together!

--- ex13.rb
+++ ex13.rb
@@ -1,5 +1,10 @@
 first, second, third = ARGV
 
-puts "Your first variable is: #{first}"
-puts "Your second variable is: #{second}"
-puts "Your third variable is: #{third}"
+puts "Your first food is: #{first}"
+puts "Your second food is: #{second}"
+puts "Your third food is: #{third}"
+
+print "what's your favourite food? "
+food = gets.chomp
+system "cls"
+puts "all your foods are #{first}, #{second}, #{third} and #{food}"
