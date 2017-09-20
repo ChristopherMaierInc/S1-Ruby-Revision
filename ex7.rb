15a5384dfbbfc3066b2b145ee7aebd340352a611
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 13:40:52 +1000

    ex7.rb complete

--- ex7.rb
+++ ex7.rb
@@ -1,8 +1,10 @@
 # prints string to the screen
 puts "Mary had a little lamb."
-# prints
+# prints string to the screen including interpolation containing another string (just to show that it can be done)
 puts "Its fleece was white as #{'snow'}."
+# prints string to the screen
 puts "And everywhere that Mary went."
+# prints the string * 10 to the screen
 puts "." * 10  # what'd that do?
 
 end1 = "C"
@@ -18,6 +20,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+# adds the variables together and either prints them or puts them to the screen: 'print' does exactly that; 'puts' prints to the screen and then moves the cursor to the line below. 
 # watch that print vs. puts on this line what's it do?
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
