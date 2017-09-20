1c31b16d40a62119822580ac17772656d840afc3
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 14:29:25 +1000

    ex8.rb complete

---
+++ ex8.rb
@@ -0,0 +1,18 @@
+# %{} creates a string in which you can have absolutely anything inside {} can be replaced with any symbol as long as it's the same either side: [] ~~ etc.
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+# puts all values of all keys of the hash to the screen while removing any formatting
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+# as above with strings
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+# as above with boolean
+puts formatter % {first: true, second: false, third: true, fourth: false}
+# puts the complete string of formatter to the screen
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+# as above by the hash is indented properly
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
