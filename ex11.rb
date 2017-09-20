43dd108f9a131c5c32a874e8c1c3bd7560616a47
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 14:52:57 +1000

    ex11.rb complete

--- ex11.rb
+++ ex11.rb
@@ -11,3 +11,6 @@ puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 
 
 # chop and chomp functions can often be a source of confusion. Not only do they sound similar, they do similar things. Unfortunately, there is a critical difference - chop removes the last character of the string completely, while chomp only removes the last character if it is a newline.
+
+# How do I get a number from someone so I can do math?
+# That's a little advanced, but try gets.chomp.to_i which says, "Get a string from the user, chomp off the \n, and convert it to an integer."
