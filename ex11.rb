421dfc0f92ddc7675154f41b7186f956de988992
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 14:52:12 +1000

    ex11.rb complete

--- ex11.rb
+++ ex11.rb
@@ -1,4 +1,6 @@
+# prints string to screen, without using \n to create a new line
 print "How old are you? "
+# gets the user input and sets it as the value in a variable - .chomp removes the last character (enter if the user presses enter)
 age = gets.chomp
 print "How tall are you? "
 height = gets.chomp
@@ -6,3 +8,6 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+# chop and chomp functions can often be a source of confusion. Not only do they sound similar, they do similar things. Unfortunately, there is a critical difference - chop removes the last character of the string completely, while chomp only removes the last character if it is a newline.
