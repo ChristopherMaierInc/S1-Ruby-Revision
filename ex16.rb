f22f9a7acb6ab62ae3181569f57add27f64f370d
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 17:32:08 +1000

    ex16.rb start

---
+++ ex16.rb
@@ -0,0 +1,10 @@
+# Exercise 16: Reading and Writing Files
+#
+# If you did the Study Drills from the last exercise, you should have seen all sorts of commands (methods/functions) you can give to files. Here's the list of commands I want you to remember:
+#
+# close -- Closes the file. Like File->Save.. in your editor.
+# read -- Reads the contents of the file. You can assign the result to a variable.
+# readline -- Reads just one line of a text file.
+# truncate -- Empties the file. Watch out if you care about the file.
+# write('stuff') -- Writes "stuff" to the file.
+# seek(0) -- Move the read/write location to the beginning of the file.
