7e83a10284b4ff8046247512c5978361e71c3e42
ChristopherMaierInc <mail@christophermaier.com.au>
2017-09-20 15:48:54 +1000

    .gets.chomp worked! ex13.rb complete!

--- ex13.rb
+++ ex13.rb
@@ -5,6 +5,6 @@ puts "Your second food is: #{second}"
 puts "Your third food is: #{third}"
 
 print "what's your favourite food? "
-food = gets.chomp
+food = $stdin.gets.chomp
 system "cls"
 puts "all your foods are #{first}, #{second}, #{third} and #{food}"
